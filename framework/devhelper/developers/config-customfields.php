bae724f1174e18c35779aa0ff3a44f9f9f400c07
Mattheus Oliveira <mattheusdeveloper@gmail.com>
2015-06-03 14:43:33 -0300

    Fix bug to custom fields link

--- framework/devhelper/developers/config-customfields.php
+++ framework/devhelper/developers/config-customfields.php
@@ -9,6 +9,10 @@
 	no seu tema já que o plugin completo está implementado no tema. Para exibir os links no menu do WordPress recomendamos que abra o arquivo 
 	<b>_framework/framework.php</b> e mude o valor da constante <b>ACF_LITE</b> na linha <b>59</b> para <b>false</b>.', 'devhelper'); ?></p>
 
-	<p><a class="button" href="<?php echo admin_url(); ?>edit.php?post_type=acf"><?php _e('Campos Personalizados', 'devhelper'); ?></a></p>
+  <?php if( is_plugin_active('advanced-custom-fields-pro/acf.php') ){ ?>
+    <p><a class="button" href="<?php echo admin_url(); ?>edit.php?post_type=acf-field-group" target="_blank"><?php _e('Campos Personalizados', 'devhelper'); ?></a></p>
+  <?php }else{ ?>
+    <p><a class="button" href="<?php echo admin_url(); ?>edit.php?post_type=acf" target="_blank"><?php _e('Campos Personalizados', 'devhelper'); ?></a></p>
+  <?php } ?>
 
 </article><!-- end .developer-custompostype -->
